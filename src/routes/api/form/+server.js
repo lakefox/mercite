2c3084b56909f1469158f4942300ae7413efcf76
LakeFox <mason@lakefox.net>
2022-09-12 20:20:05 -0500

    rmt ry

--- src/routes/api/form/+server.js
+++ src/routes/api/form/+server.js
@@ -10,23 +10,20 @@ const client = new MongoClient(mongoURI);
 export async function POST({ params, request }) {
     const database = client.db('InstanceOne');
     const body = await request.formData(); // or request.json(), etc
-    try {
-        const table = body.get("table");
-        const tableDB = database.collection(table);
+    const table = body.get("table");
+    const tableDB = database.collection(table);
 
-        let keys = [...body.entries()];
-        let doc = {};
+    let keys = [...body.entries()];
+    let doc = {};
 
-        for (let i = 0; i < keys.length; i++) {
-            const element = keys[i];
-            if (["redirect", "table"].indexOf(element[0]) == -1) {
-                doc[element[0]] = element[1];
-            }
+    for (let i = 0; i < keys.length; i++) {
+        const element = keys[i];
+        if (["redirect", "table"].indexOf(element[0]) == -1) {
+            doc[element[0]] = element[1];
         }
-
-        await tableDB.insertOne(doc);
-    } finally {
-        await client.close();
-        throw redirect(307, body.get("redirect") || "/");
     }
+
+    await tableDB.insertOne(doc);
+    await client.close();
+    throw redirect(307, body.get("redirect") || "/");
 }
